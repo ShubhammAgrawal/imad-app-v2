ac5672de8d6f0d4fc4da5cc5744bfc0dd50a9b17
Shubham Agrawal <ShubhammAgrawal@users.noreply.github.com>
2017-02-08 18:02:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/download.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'download.jpg'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
